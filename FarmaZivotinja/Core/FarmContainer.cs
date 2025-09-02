1357d49f948c43e46451f956971631061ecd5994
Ivan Gluhak <igluhak@vub.hr>
2025-09-02 17:12:27 +0200

    maknut kontejner koji se koristio u prosloj verziji na laptopu

--- FarmaZivotinja/Core/FarmContainer.cs
+++ FarmaZivotinja/Core/FarmContainer.cs
@@ -1,14 +1,1 @@
-﻿using System.Collections.Generic;
-
-namespace FarmaZivotinja.Core
-{
-   
-    public class FarmContainer<T>
-    {
-        private readonly List<T> items = new();
-
-        public void Dodaj(T item) => items.Add(item);
-        public IEnumerable<T> DohvatiSve() => items;
-        public int BrojStavki => items.Count;
-    }
-}
+﻿
